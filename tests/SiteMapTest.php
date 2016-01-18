bb91c5e25da0bd8d82fac1cc8efef80923a36027
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-19 01:46:55 +0700

    WIP: Initial tests shell

---
+++ tests/SiteMapTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class SiteMapTest extends SapphireTest {
+	public function testSiteMap() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testMakeList() {
+		$this->markTestSkipped('TODO');
+	}
+
+}
