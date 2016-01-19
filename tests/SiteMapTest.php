3b90b502728f75fc7e0210a53273413858730b08
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-19 11:29:07 +0700

    ENHANCEMENT: First cut of tests

--- tests/SiteMapTest.php
+++ tests/SiteMapTest.php
@@ -1,12 +1,23 @@
 <?php
 
-class SiteMapTest extends SapphireTest {
-	public function testSiteMap() {
-		$this->markTestSkipped('TODO');
-	}
+class SiteMapTest extends FunctionalTest
+{
+    protected static $fixture_file = 'SiteMapTest.yml';
 
-	public function testMakeList() {
-		$this->markTestSkipped('TODO');
-	}
-
+    public function testSiteMap()
+    {
+        $this->logInWithPermission('ADMIN');
+        // pages need published, fixtures are not
+		foreach (Page::get() as $page) {
+			$page->doPublish();
+		}
+		$response = $this->get('/sitemap/');
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = $response->getBody();
+        for ($i=1; $i <= 4; $i++) {
+        	$row = '<li class="link"><a href="/page-' . $i . '/" title="Page '
+        	. $i . '">Page ' . $i . '</a></li>';
+        	$this->assertContains($row, $body);
+        }
+    }
 }
