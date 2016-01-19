a4b9418d0545d51af16b50dd23956ea2db1a9e51
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-19 11:44:23 +0700

    ENHANCEMENT: Assert order

--- tests/SiteMapTest.php
+++ tests/SiteMapTest.php
@@ -13,11 +13,19 @@ class SiteMapTest extends FunctionalTest
 		}
 		$response = $this->get('/sitemap/');
         $this->assertEquals(200, $response->getStatusCode());
+        $positions = array();
         $body = $response->getBody();
         for ($i=1; $i <= 4; $i++) {
         	$row = '<li class="link"><a href="/page-' . $i . '/" title="Page '
         	. $i . '">Page ' . $i . '</a></li>';
         	$this->assertContains($row, $body);
+
+        	$positions["{$i}"] = strpos($body, $row);
         }
+
+        //assert order
+        $this->assertGreaterThan($positions['3'], $positions['4']);
+        $this->assertGreaterThan($positions['2'], $positions['3']);
+        $this->assertGreaterThan($positions['1'], $positions['2']);
     }
 }
