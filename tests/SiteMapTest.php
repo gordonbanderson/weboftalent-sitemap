ecf4a5708404b563503335c2708d8b50b47cf2c6
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-19 13:34:48 +0700

    ENHANCEMENT: Checks for a nested level

--- tests/SiteMapTest.php
+++ tests/SiteMapTest.php
@@ -10,7 +10,6 @@ class SiteMapTest extends FunctionalTest
         // pages need published, fixtures are not
 		foreach (Page::get() as $page) {
 			$page->doPublish();
-			error_log($page->ClassName . ':' . $page->Link());
 		}
 		$response = $this->get('/sitemap/');
         $this->assertEquals(200, $response->getStatusCode());
@@ -18,13 +17,13 @@ class SiteMapTest extends FunctionalTest
         $body = $response->getBody();
 
         // assert is a sitemap
-       	$this->assertContains('<ul class="sitemap-list">', $body);
+        $count = substr_count($body, '<ul class="sitemap-list">');
+       	$this->assertEquals(2, $count);
 
        	// assert root level pages
         for ($i=1; $i <= 4; $i++) {
         	$row = '<li><a href="page-' . $i . '" title="Go to the Page ' . $i . ' page">Page ' . $i . '</a>';
         	$this->assertContains($row, $body);
-
         	$positions["{$i}"] = strpos($body, $row);
         }
 
@@ -32,5 +31,15 @@ class SiteMapTest extends FunctionalTest
         $this->assertGreaterThan($positions['3'], $positions['4']);
         $this->assertGreaterThan($positions['2'], $positions['3']);
         $this->assertGreaterThan($positions['1'], $positions['2']);
+
+        // check sub pages
+        $sub1 = '<li><a href="page-11" title="Go to the Page 1/1 page">Page 1/1</a>';
+        $sub2 = '<li><a href="page-12" title="Go to the Page 1/2 page">Page 1/2</a>';
+        $sub1pos = strpos($body, $sub1);
+        $sub2pos = strpos($body, $sub2);
+        $this->assertContains($sub1, $body);
+        $this->assertContains($sub2, $body);
+        $this->assertGreaterThan($positions[1], $sub1pos);
+        $this->assertGreaterThan($sub1pos, $sub2pos);
     }
 }
